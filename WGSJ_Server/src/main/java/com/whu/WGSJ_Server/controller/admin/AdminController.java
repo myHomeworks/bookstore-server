b252756e4f9e2987bc34f6cbcb6952a59b6d939a
lugf027 <161230578@qq.com>
2020-04-14 23:49:25 +0800

    some simple controller🐣

---
+++ WGSJ_Server/src/main/java/com/whu/WGSJ_Server/controller/admin/AdminController.java
@@ -0,0 +1,54 @@
+package com.whu.WGSJ_Server.controller.admin;
+
+import com.alibaba.fastjson.JSONObject;
+import com.whu.WGSJ_Server.domain.Admin;
+import com.whu.WGSJ_Server.domain.User;
+import com.whu.WGSJ_Server.service.AdminService;
+import com.whu.WGSJ_Server.service.RedisService;
+import com.whu.WGSJ_Server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/admin")
+@Component("AdminController")
+public class AdminController {
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RedisService redisService;
+
+    @PostMapping("/loginByWeChat")
+    public HashMap<String, Object> loginByWeChat(@RequestBody String body){
+        HashMap<String, Object> ret = new HashMap<>();
+        JSONObject object = JSONObject.parseObject(body);
+        String adminId = object.getString("adminId");
+        String weChatSessionId = object.getString("weChatSessionId");
+        if(!userService.getUserById(adminId).getSessionId().equals(weChatSessionId)){
+            ret.put("state", "failAuthentication");
+            return ret;
+        }
+        Admin admin = adminService.getAdminById(adminId);
+        redisService.delSessionId(admin.getSessionId());
+        String adminSessionId = UUID.randomUUID().toString();
+        admin.setSessionId(adminSessionId);
+        if(adminService.updateAdmin(admin) != 1){
+            ret.put("state", "failUpdate");
+            return ret;
+        }
+        ret.put("sessionId", adminSessionId);
+        ret.put("state", "ok");
+        return ret;
+    }
+}
--- WGSJ_Server/src/main/java/com/whu/WGSJ_Server/controller/user/UserController.java
+++ WGSJ_Server/src/main/java/com/whu/WGSJ_Server/controller/user/UserController.java
@@ -4,9 +4,15 @@ import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whu.WGSJ_Server.config.APIConfig;
+import com.whu.WGSJ_Server.domain.Admin;
 import com.whu.WGSJ_Server.domain.User;
+import com.whu.WGSJ_Server.service.AdminService;
+import com.whu.WGSJ_Server.service.ConfigService;
 import com.whu.WGSJ_Server.service.RedisService;
 import com.whu.WGSJ_Server.service.UserService;
+import net.sf.jsqlparser.expression.operators.relational.JsonOperator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +31,6 @@ import java.util.UUID;
 @RequestMapping("/user/user")
 @Component("UserController")
 public class UserController {
-
     @Autowired
     private ObjectMapper mapper;
 
@@ -35,75 +40,95 @@ public class UserController {
     @Autowired
     private RedisService redisService;
 
-//    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//
-//    @PostMapping("/login")
-//    public HashMap<String, String> login(@RequestBody String body) throws IOException {
-//        JSONObject object = JSONObject.parseObject(body);
-//        HashMap<String, String> ret = new HashMap<>(); // 返回值
-//        String code = (String) object.get("code");
-//        JSONObject userInfo = (JSONObject) object.get("userInfo");
-//        Double latitude = object.getBigDecimal("latitude").doubleValue();
-//        Double longitude = object.getBigDecimal("longitude").doubleValue();
-//        RestTemplate restTemplate = new RestTemplate();// 发送request请求
-//        String params = "?appid=" + APIConfig.wxspAppid + "&secret=" + APIConfig.wxspSecret + "&js_code=" + code + "&grant_type=authorization_code";//参数
-//        String url = APIConfig.openIdURL + params;
-//        String response = restTemplate.getForObject(url, String.class);
-//
-//        JsonNode node = this.mapper.readTree(response);
-//        String openid = node.get("openid").asText();
-////        String sessionKey = node.get("session_key").asText(); // 微信的session_key
-//
-//        User user = new User();
-//        user.setUserId(openid);
-//        user.setLatitude(latitude != null ? latitude : 0.0);
-//        user.setLongitude(longitude != null ? longitude : 0.0);
-//
-//        parseJson2User(userInfo, user);
-//        String userSessionKey = UUID.randomUUID().toString(); // 我们的sessionKey
-//        user.setSessionId(userSessionKey);
-//        User check = userService.getUserById(openid);
-//        if (check == null) {
-//            user.setTime(dateFormat.format(new Date()));
-//            if (userService.register(user) == 1)
-//                check = user;
-//            else {
-//                ret.put("state", "register fail!");
-//                return ret;
-//            }
-//        } else {
-//            redisService.delSessionId(check.getSessionId());
-//            updateFromWeixin(check, user);
-//            if (userService.updateUser(check) == 1)
-//                check = user;
-//            else {
-//                ret.put("state", "login fail!");
-//                return ret;
-//            }
-//        }
-//        redisService.saveUserOrAdminBySessionId(userSessionKey, check);
-//
-//        ret.put("state", "ok");
-//        ret.put("openId", openid);
-//        ret.put("sessionKey", userSessionKey);
-//        return ret;
-//    }
-//
-//    private void parseJson2User(JSONObject userinfo, User user) {
-//        user.setName((String) userinfo.get("nickName"));
-//        user.setNickname((String) userinfo.get("nickName"));
-//        user.setGender((Integer) userinfo.get("gender"));
-//        user.setAvatar((String) userinfo.get("avatarUrl"));
-//    }
-//
-//
-//    private void updateFromWeixin(User check, User user) {
-//        check.setName(user.getName());
-//        check.setGender(user.getGender());
-//        check.setAvatar(user.getAvatar());
-//        check.setLongitude(user.getLongitude());
-//        check.setLatitude(user.getLatitude());
-//        check.setSessionId(user.getSessionId());
-//    }
+    @Autowired
+    private ConfigService configService;
 
+    @Autowired
+    private AdminService adminService;
+
+    private static String appId;
+    private static String secret;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @PostMapping("/login")
+    public HashMap<String, Object> login(@RequestBody String body) throws IOException {
+        if (UserController.appId == null) {
+            UserController.appId = configService.getConfigByKey("appId").getValue();
+            UserController.secret = configService.getConfigByKey("secret").getValue();
+            log.info("appId:\t\t" + UserController.appId);
+        }
+
+        HashMap<String, Object> ret = new HashMap<>(); // 返回值
+        JSONObject object = JSONObject.parseObject(body);
+
+        String openId = this.getOpenIdFromCode(object.getString("code"));
+        User user = userService.getUserById(openId);
+
+        if (user == null) { // 创建新用户
+            user = this.buildUserNew(object, openId);
+            if (userService.addUser(user) != 1) {
+                ret.put("state", "failAdd");
+                return ret;
+            }
+        } else { // 更新老用户
+            redisService.delSessionId(user.getSessionId());
+            this.updateUser(user, object);
+            if (userService.updateUser(user) != 1) {
+                ret.put("state", "failUpdate");
+                return ret;
+            }
+            // 判断用户是否为管理员
+            if (adminService.getAdminById(openId) != null) {
+                ret.put("isAdmin", true);
+            }
+        }
+
+        redisService.saveObjectInstanceBySessionId(user.getSessionId(), user);
+
+        ret.put("state", "ok");
+        ret.put("userId", user.getUserId());
+        ret.put("sessionId", user.getSessionId());
+        return ret;
+    }
+
+    // 发送微信request请求,通过code获得openId
+    private String getOpenIdFromCode(String code) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String params = "?appid=" + appId + "&secret=" + secret + "&js_code=" + code + "&grant_type=authorization_code";
+        String url = APIConfig.openIdURL + params;
+        String response = restTemplate.getForObject(url, String.class);
+
+        JsonNode node = this.mapper.readTree(response);
+        return node.get("openid").asText();
+    }
+
+    // 新注册时创建新用户
+    private User buildUserNew(JSONObject object, String openid) {
+        User user = new User();
+
+        user.setUserId(openid);
+        user.setTime(dateFormat.format(new Date()));
+
+        this.updateUser(user, object);
+        user.setNickname(user.getName());
+        return user;
+    }
+
+    // 更新老用户的部分信息
+    private void updateUser(User user, JSONObject object) {
+        JSONObject userInfo = (JSONObject) object.get("userInfo");
+        user.setName(userInfo.getString("nickName"));
+        user.setGender(userInfo.getInteger("gender"));
+        user.setAvatar(userInfo.getString("avatarUrl"));
+
+        JSONObject location = (JSONObject) object.get("location");
+        double latitude = location.getBigDecimal("latitude").doubleValue();
+        double longitude = location.getBigDecimal("longitude").doubleValue();
+        user.setLatitude(latitude);
+        user.setLongitude(longitude);
+
+        String userSessionId = UUID.randomUUID().toString();
+        user.setSessionId(userSessionId);
+    }
 }
