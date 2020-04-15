862e73ba49bc501626c8a2522248af703deadf2b
lugf027 <161230578@qq.com>
2020-04-15 18:34:45 +0800

    class of book🐣

---
+++ WGSJ_Server/src/main/java/com/whu/WGSJ_Server/controller/admin/BookClassController.java
@@ -0,0 +1,128 @@
+package com.whu.WGSJ_Server.controller.admin;
+
+import com.alibaba.fastjson.JSONObject;
+import com.whu.WGSJ_Server.domain.Classification;
+import com.whu.WGSJ_Server.service.ClassificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@RequestMapping("/admin/bookClass")
+@Component("AdminBookClassController")
+public class BookClassController {
+    @Autowired
+    private ClassificationService classService;
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @PostMapping("/all")
+    public HashMap<String, Object> all(@RequestBody String body) {
+        HashMap<String, Object> ret = new HashMap<>();
+        List<Classification> fathers = classService.getAllFathers();
+        List<ClassList> classLists = new ArrayList<>();
+        for(Classification father: fathers){
+            ClassList classList = new ClassList();
+            classList.setId(father.getClassId());
+            classList.setName(father.getName());
+            List<Classification> children = classService.getAllChildrenByFatherId(father.getClassId());
+            List<String> names = new ArrayList<>();
+            List<String> ids = new ArrayList<>();
+            for(Classification child: children){
+                names.add(child.getName());
+                ids.add(child.getClassId());
+            }
+            classList.setPages(names);
+            classList.setPagesId(ids);
+            classLists.add(classList);
+        }
+        ret.put("list", classLists);
+        ret.put("state", "ok");
+        return ret;
+    }
+
+    @PostMapping("/add")
+    public HashMap<String, Object> add(@RequestBody String body) {
+        HashMap<String, Object> ret = new HashMap<>();
+        JSONObject object = JSONObject.parseObject(body);
+        Classification classification = new Classification();
+        String fatherName = object.getString("fatherName");
+        if(fatherName == null || fatherName.equals("")){
+            classification.setIfFather(1);
+        }else{
+            Classification fatherClass = classService.getFatherClassByName(fatherName);
+            if (fatherClass == null) {
+                ret.put("state", "noFather");
+                return ret;
+            }
+            classification.setFatherId(fatherClass.getClassId());
+            classification.setIfFather(0);
+        }
+
+        classification.setClassId(UUID.randomUUID().toString());
+        classification.setName(object.getString("name"));
+        classification.setTime(dateFormat.format(new Date()));
+        if (classService.addClass(classification) != 1)
+            ret.put("state", "failAdd");
+        else
+            ret.put("state", "ok");
+        return ret;
+    }
+
+    @PostMapping("/delete")
+    public HashMap<String, Object> delete(@RequestBody String body) {
+        HashMap<String, Object> ret = new HashMap<>();
+        JSONObject object = JSONObject.parseObject(body);
+        String classId = object.getString("classId");
+        if (classService.deleteClassById(classId) == 1)
+            ret.put("state", "ok");
+        else
+            ret.put("state", "failDelete");
+        return ret;
+    }
+}
+
+class ClassList {
+    private String id;
+    private String name;
+    private List<String> pages;
+    private List<String> pagesId;
+
+    public List<String> getPagesId() {
+        return pagesId;
+    }
+
+    public void setPagesId(List<String> pagesId) {
+        this.pagesId = pagesId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPages() {
+        return pages;
+    }
+
+    public void setPages(List<String> pages) {
+        this.pages = pages;
+    }
+}
